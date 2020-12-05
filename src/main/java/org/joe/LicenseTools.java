e9856e67bcc6467b13486eda5257ac394868c577
home <home@DESKTOP-VQBCJ28>
2020-12-05 09:11:38 +0800

    update LicenseTools

--- src/main/java/org/joe/LicenseTools.java
+++ src/main/java/org/joe/LicenseTools.java
@@ -13,6 +13,7 @@ public class LicenseTools {
         }
         try {
             loadAsposeWordLicense();
+            loadAsposeCellLicense();
             isLicenseLoaded = true;
         } catch (Exception ex) { // ignore error
         }
@@ -23,6 +24,16 @@ public class LicenseTools {
         try (InputStream licStream = LicenseTools.class.getResourceAsStream(licPath)) {
             com.aspose.words.License wordsLicense = new com.aspose.words.License();
             wordsLicense.setLicense(licStream);
+        } catch (Exception ex) {
+            System.out.println("License not found!");
+        } finally {
+        }
+    }
+
+    private static void loadAsposeCellLicense() throws Exception {
+        try (InputStream licStream = LicenseTools.class.getResourceAsStream(licPath);) {
+            com.aspose.cells.License cellsLicense = new com.aspose.cells.License();
+            cellsLicense.setLicense(licStream);
         } catch (Exception ex) {
             System.out.println("License not found!");
         } finally {
